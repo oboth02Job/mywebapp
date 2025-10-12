637178e8084ebb101dcacc9cc62c8f4df3aa2a63
Job Oboth <trappsavage601@gmail.com>
2025-10-12 22:43:15 +0300

    Modified some files

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import "./progress.js";
 
 
 const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector("nav");
+const nav = document.querySelector("nav-links");
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active")
 })
+
+
